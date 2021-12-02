0b3420a46c680b6e81667a7911ec1ac499b03ae8
Parranoh <minerva.schlau@hotmail.com>
2021-12-02 01:30:06 +0100

    be smart for 1b

--- 01/b.sql
+++ 01/b.sql
@@ -5,17 +5,9 @@ CREATE TABLE input (
 );
 \copy input(n) FROM PSTDIN
 
-WITH three AS (
-    SELECT
-        id,
-        sum(n) OVER w AS sum,
-        count(*) OVER w as count
+WITH inc AS (
+    SELECT lag(n, 3) OVER (ORDER BY id) < n AS increased
     FROM input
-    WINDOW w AS (ORDER BY id ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
-), inc AS (
-    SELECT lag(sum) OVER (ORDER BY id) < sum AS increased
-    FROM three
-    WHERE count = 3
 )
 SELECT count (*)
 FROM inc
