882981600333706f414be49216436f924c8ab074
Parranoh <minerva.schlau@hotmail.com>
2021-12-12 07:25:17 +0100

    cache all_lower_distinct for each path

--- 12/b.sql
+++ 12/b.sql
@@ -1,18 +1,6 @@
 CREATE TEMPORARY TABLE input(line text NOT NULL);
 \copy input(line) from pstdin
 
-CREATE OR REPLACE FUNCTION all_lower_distinct(a text[]) RETURNS bool AS
-$$
-    SELECT coalesce(bool_and(a1.v <> a2.v), TRUE)
-    FROM unnest(a) WITH ORDINALITY AS a1(v,ix),
-        LATERAL unnest(a) WITH ORDINALITY AS a2(v,ix)
-    WHERE a1.ix <> a2.ix
-        AND a1.v = lower(a1.v)
-        AND a2.v = lower(a2.v)
-$$
-LANGUAGE SQL
-IMMUTABLE;
-
 WITH RECURSIVE
 edges(tail, head, "to_big?") AS (
     SELECT tail, head, upper(head) = head
@@ -21,15 +9,15 @@ edges(tail, head, "to_big?") AS (
         LATERAL (VALUES (e[1], e[2]), (e[2], e[1])) AS __(tail, head)
     WHERE tail <> 'end' AND head <> 'start'
 ),
-paths(p) AS (
-    SELECT ARRAY['start']
+paths(p, "duplicate?") AS (
+    SELECT ARRAY['start'], FALSE
 UNION ALL
-    SELECT p || head
+    SELECT p || head, "duplicate?" OR NOT ("to_big?" OR head <> ALL(p))
     FROM paths
         JOIN edges ON p[array_length(p, 1)] = tail
     WHERE "to_big?"
+        OR NOT "duplicate?"
         OR head <> ALL(p)
-        OR all_lower_distinct(p)
 )
 SELECT count(*)
 FROM paths
